afd44b2baad857f06d8783e421b62f601c451b6d
PSAN1987 <88932722+PSAN1987@users.noreply.github.com>
2025-02-01 14:48:38 +0900

    Add files via upload

--- graffitees_LINE_BOT.py
+++ graffitees_LINE_BOT.py
@@ -74,9 +74,18 @@ def create_flex_message():
             "layout": "vertical",
             "contents": [
                 {"type": "text", "text": "モードを選択してください", "weight": "bold", "size": "lg"},
-                {"type": "button", "style": "primary", "action": {"type": "postback", "label": "簡易見積", "data": "quick_estimate"}},
-                {"type": "button", "style": "primary", "action": {"type": "postback", "label": "WEBフォームから注文", "data": "web_order"}},
-                {"type": "button", "style": "primary", "action": {"type": "postback", "label": "注文用紙から注文", "data": "paper_order"}}
+                {
+                    "type": "button", "style": "primary", 
+                    "action": {"type": "postback", "label": "簡易見積", "data": "quick_estimate"}
+                },
+                {
+                    "type": "button", "style": "primary", 
+                    "action": {"type": "postback", "label": "WEBフォームから注文", "data": "web_order"}
+                },
+                {
+                    "type": "button", "style": "primary", 
+                    "action": {"type": "postback", "label": "注文用紙から注文", "data": "paper_order"}
+                }
             ]
         }
     }
